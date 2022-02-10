482d9af3eab54065921dc99ab138d9e9342a21a4
Admin <Admin@DESKTOP-9H2KH00>
2022-02-10 14:38:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQ = new Queue(); 
+
+            myQ.Enqueue(1);
+            myQ.Enqueue(14);
+            myQ.Enqueue(3);
+            myQ.Enqueue(46);
+            myQ.Enqueue(8);
+                                    
+            PrintValues(myQ);
+                        
+            Console.WriteLine("(Dequeue)\t{0}", myQ.Dequeue());
+            PrintValues(myQ);
+                            
+            Console.WriteLine("(Peek)   \t{0}", myQ.Peek());
+            PrintValues(myQ);
+        }
+
+        public static void PrintValues(Queue myQ)
+        {
+            
+            Console.Write("    {0}", myQ);
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+    }
+
+}
+
---
+++ ConsoleApp11/Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Queue : IQiueue
+    {
+        private List<int> queue = new List<int>();
+
+
+        public int Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Peek()
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
